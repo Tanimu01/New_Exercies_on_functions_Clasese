0f346d846caa946e717636340cf1c8b6e075662c
Tanimu Abdullahi <aaltanim@gmail.com>
2023-02-12 01:43:43 +0100

    i aded function make_shirt

--- New_Exersies_function.py
+++ New_Exersies_function.py
@@ -16,3 +16,14 @@ def favorite_book(book_title):
     print('favorite books is Alice in', book_title)
 
 favorite_book('Wonderland')
+
+# 8-3. T-Shirt: Write a function called make_shirt() that accepts a size and the
+# text of a message that should be printed on the shirt. The function should print
+#a sentence summarizing the size of the shirt and the message printed on it.
+#Call the function once using positional arguments to make a shirt. Call the
+# function a second time using keyword arguments.
+
+def  make_shirt(size,text):
+    print('this is my size: ' , size,' and the text to be printed is',text)
+
+make_shirt(14,'EELTANIM')
