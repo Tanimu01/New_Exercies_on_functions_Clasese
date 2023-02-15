700b172acb804b5bd20aec42e96baeab78ebd798
Tanimu Abdullahi <aaltanim@gmail.com>
2023-02-15 17:08:17 +0100

    new

---
+++ printing_model.py
@@ -0,0 +1,7 @@
+import printing_functions
+
+rt = ['phone case', 'robot pendant', 'dodecahedron', 'Tanimu']
+completed_models = []
+
+printing_functions.print_models(rt,completed_models)
+printing_functions.show_completed_models(completed_models)
